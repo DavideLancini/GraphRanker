1deedb0fe98d7ffc4441148d0f6b441da9dfac2a
Davide Lancini <davide1.lancini@mail.polimi.it>
2021-07-25 21:10:14 +0200

    Added Main Class

---
+++ API.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+  printf("test");
+  return;
+}
+
+// first input d (numero nodi grafo) int 32bit
+    //k lunghezza classifica int 32bit
+
+//AggiungiGrafo [matrice adiacenza]
+    //ricevi grafo
+    //valuta grafo
+    //aggiorna Top se necessario
+
+//TopK stampa indici(da zero) dei migliori finora (separati da spazio)
