9e9f23b48cc98b2cef271368736985341846b9f0
DavideLancini <davide1.lancini@mail.polimi.it>
2021-09-03 13:07:33 +0200

    CleanUp: variables & function calls

--- API.c
+++ API.c
@@ -1,100 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readGraph(int *graphMatrix);
-void printGraph(int *graphMatrix);
-void evaluateGraph(int *graphMatrix);
+typedef struct node {
+  int index;
+  int evaluation;
+  struct node *next;
+} Node;
 
-void printTop(int *topK);
-void addTop(int *topK);
+void readGraph(int dimension, int *graphMatrix);
+void printGraph(int dimension, int *graphMatrix);
+int evaluateGraph(int dimension, int *graphMatrix);
 
-int d,k,evaluation,graphIndex,trashold,maxLine;
-char command[14];
+int topIsSaturated;
+void printTop(Node *topHead);
+void addTop(int index, int evaluation, Node *topHead);
 
 void main(){
+  int d,k,evaluation,graphIndex,trashold,maxLine;
+  char command[14];
+
   //read parameters
   scanf("%d %d", &d, &k);
   maxLine = 12*d;
   graphIndex = 0;
   trashold = 0;
+  topIsSaturated = 0;
 
   //allocate necessary structures
   int *graphMatrix = (int *) malloc (d * d * sizeof (int));
-  int *topK = (int *) malloc (2*k * sizeof (int)); //index, evaluation
+  Node *topHead = NULL;
 
   while(1){
     scanf("%s", command);
     if(command[0] == 'T'){ //TopK
-      printTop(topK);
+      printTop(topHead);
     }
     else{ //AggiungiGrafo
-      readGraph(graphMatrix);
+      readGraph(d, graphMatrix);
       //printGraph(graphMatrix); //DEBUG
-      evaluateGraph(graphMatrix);
-      if(evaluation >= trashold){
-        addTop(topK);
-      }
+      evaluation = evaluateGraph(d, graphMatrix);
+      addTop(graphIndex, evaluation, topHead);
       graphIndex++;
+      if(graphIndex = k-1) //the list is filled
+        topIsSaturated=1;
     }
   }
   return; //Unreachable
 }
 
-void readGraph(int *graphMatrix){
+void readGraph(int dimension, int *graphMatrix){
   char *ptr;
-  char st[maxLine];
+  char st[dimension * dimension];
   long ret;
-  for (int row = 0; row < d; row++){
+  for (int row = 0; row < dimension; row++){
     scanf("%s", st);
     //fgets(st, maxLine, stdin); COULD BE BETTER
     ptr = &st[0];
-    for (int column = 0; column < d; column++){
+    for (int column = 0; column < dimension; column++){
       ret = strtol(ptr, &ptr, 10);
       ptr = ptr + sizeof (char);
-      *(graphMatrix + row * d + column) = (int)ret;
+      *(graphMatrix + row * dimension + column) = (int)ret;
       if(row == column || column == 0)
-        *(graphMatrix + row * d + column) = 0; //diagonal and column 0 is useless for our usage
+        *(graphMatrix + row * dimension + column) = 0; //diagonal and column 0 is useless for our usage
     }
   }
 }
 
-void printGraph(int *graphMatrix){
+void printGraph(int dimension, int *graphMatrix){
   printf("\nPRINT: \n");
-  for (int row = 0; row < d; row++){
-    for (int column = 0; column < d; column++){
-      printf("%d ", *(graphMatrix + row * d + column));
+  for (int row = 0; row < dimension; row++){
+    for (int column = 0; column < dimension; column++){
+      printf("%d ", *(graphMatrix + row * dimension + column));
     }
     printf("\n");
   }
 }
 
-void evaluateGraph(int *graphMatrix){
+int evaluateGraph(int dimension, int *graphMatrix){
+  int evaluation = 0;
   //allocate activationVector
-  int *activeRow = (int *) malloc (d * sizeof (int)); //store the active rows
+  int *activeRow = (int *) malloc (dimension * sizeof (int)); //store the active rows
   //TODO: better way for active rows: store the index (one less for cycle)
   int min = 0;
   int minPos[2];
   //reset activation vector
   *(activeRow) = 1;
-  for(int i=1;i<d;i++)
+  for(int i=1;i<dimension;i++)
     *(activeRow + i) = 0;
   //SETUP
-  for(int i=1;i<d;i++){
+  for(int i=1;i<dimension;i++){
     if(*(graphMatrix + i) != 0){ //finding the first non zero value in the row 0
       min = *(graphMatrix + i);
       minPos[0]=0;
       minPos[1]=i;
       break;
     }
-    if(i==d-1){ //reached when row 0 is filled with zero, no connection possible
-      evaluation = 0;
-      return;
+    if(i==dimension-1){ //reached when row 0 is filled with zero, no connection possible
+      return 0;
     }
   }
 
   //MAIN CYCLE
-  for(int i = 0; i<d;i++){ //cycle in dimension
-    for(int j = 0; i<d;i++){ //cycle in active rows
+  for(int i = 0; i<dimension;i++){ //cycle in dimension
+    for(int j = 0; i<dimension;i++){ //cycle in active rows
       if(*(activeRow + j) == 1){ //if the row is active
         //TODO:cycle in that row for the minimum
       }
@@ -105,23 +113,21 @@ void evaluateGraph(int *graphMatrix){
     //TODO:add minimum value to the row
     //TODO:update evaluation
   }
-  return;
+  return evaluation;
 }
 
-void printTop(int *topK){
-  for(int i = 0; i<graphIndex && i<k;i++){
-    printf ("%d ", *(topK + i * 2));
-  }
+void printTop(Node *topHead){
+    Node *ptr = topHead;
+    while(ptr!=NULL){
+        printf("%d ", ptr->index);
+        ptr=ptr->next;
+    }
 }
 
-void addTop(int *topK){
+void addTop(int index, int evaluation, Node *topHead){
   //Se grafici meno di k inserisco in fondo
-
-  *(TopK + 2*graphIndex) = graphIndex; //Insert on bottom
-
   //Se grafici più di k
   //Search from bottom
   //Change founded with current
   //Search for new minimum trashold
-
 }
